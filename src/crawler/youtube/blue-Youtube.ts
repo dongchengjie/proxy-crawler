88db5dbaae81d702fccda2b636b9796b34f017fb
dongchnegjie <37543964+dongchengjie@users.noreply.github.com>
2026-04-14 02:54:23 +0800

    chore: update blue-Youtube

--- src/crawler/youtube/blue-Youtube.ts
+++ src/crawler/youtube/blue-Youtube.ts
@@ -1,10 +1,10 @@
 import { Crawler } from "@/crawler/crawler.ts";
-import { charCombinations } from "@/util/password.ts";
+// import { charCombinations } from "@/util/password.ts";
 import { getBody } from "@/util/http.ts";
-import { runScript } from "@/util/vm.ts";
+// import { runScript } from "@/util/vm.ts";
 import * as cheerio from "cheerio";
 // @ts-types="npm:@types/crypto-js"
-import CryptoJS from "crypto-js";
+// import CryptoJS from "crypto-js";
 
 export class BlueYoutube extends Crawler {
   public override name(): string {
@@ -12,68 +12,47 @@ export class BlueYoutube extends Crawler {
   }
 
   public override async getFileContent(): Promise<string | undefined> {
-    // 获取博客列表
-    const blogs = await getBody("https://blues2022.blogspot.com/");
-    if (!blogs) return;
-
-    // 获取最新博客链接
-    const $blogs = cheerio.load(blogs);
-    const blogUrl = $blogs(".entry-title > a")
-      .toArray()
-      .filter((el) => $blogs(el).text().includes("免费节点"))
-      .map((el) => $blogs(el).attr("href"))
-      .find(Boolean);
-    if (!blogUrl) return;
-    this.log(`最新博客链接: ${blogUrl}`);
-
-    // 获取博客内容
-    const blog = await getBody(blogUrl);
+    // 获取网页内容
+    const blog = await getBody("https://us1.zhuk.dpdns.org/13-Youtube.html");
     if (!blog) return;
 
-    // 获取订阅链接加密后的内容
-    const $blog = cheerio.load(blog);
-    const es = $blog("script")
-      .map((_index, el) => $blog(el).text())
-      .toArray()
-      .filter(Boolean)
-      .find((text) => text.includes("var encryption"));
-    const ec = {};
-    runScript(es, ec);
+    // 获访问密码
+    // const $blog = cheerio.load(blog);
+    // const es = $blog("#cYaml")
+    //   .map((_index, el) => $blog(el).text())
+    //   .toArray()
+    //   .filter(Boolean)
+    //   .find((text) => text.includes("const pwdHash"));
+    // const ec = {};
+    // runScript(es, ec);
 
-    // @ts-ignore 获取 `encryption` 字段
-    const encryption: string = (ec?.encryption ?? [])?.find(Boolean);
-    if (!encryption) return;
-    this.log(`encryption字段: ${encryption}`);
+    // // @ts-ignore 获取 `pwdHash` 字段
+    // const pwdHash: string = (ec?.pwdHash ?? [])?.find(Boolean);
+    // if (!pwdHash) return;
+    // this.log(`pwdHash字段: ${pwdHash}`);
 
-    // 暴力解密
-    const passwords = charCombinations("0123456789", 4, 6);
-    let decryption: string | undefined;
-    for (const attempt of passwords) {
-      try {
-        const aes = CryptoJS.AES.decrypt(encryption, attempt);
-        const decoded = (decryption = decodeURIComponent(
-          aes.toString(CryptoJS.enc.Utf8),
-        ));
-        if (decoded != null && decoded.length > 0) {
-          decryption = decoded;
-          this.log(`解密密码: ${attempt}`);
-          break;
-        }
-      } catch {
-        // ignore
-      }
-    }
-    if (!decryption) return;
+    // // 暴力解密
+    // const passwords = charCombinations("0123456789", 4, 6);
+    // let password: string | undefined;
+    // for (const attempt of passwords) {
+    //   try {
+    //     const hash = CryptoJS.SHA256(attempt).toString();
+    //     if (hash === pwdHash) {
+    //       password = attempt;
+    //       this.log(`访问密码: ${attempt}`);
+    //       break;
+    //     }
+    //   } catch {
+    //     // ignore
+    //   }
+    // }
+    // if (!password) return;
 
     // 获取订阅链接
-    const $decryption = cheerio.load(decryption);
-    let subscriptionUrl = $decryption("p")
+    const $blog = cheerio.load(blog);
+    let subscriptionUrl = $blog("#cYaml")
       .toArray()
-      .map((el) => {
-        if ($decryption(el).text().toLowerCase().endsWith(".yaml")) {
-          return $decryption(el).text();
-        }
-      })
+      .map((el) => $blog(el).text().trim())
       .find(Boolean);
     subscriptionUrl = subscriptionUrl?.match(/https?:\/\/\S+/)?.[0];
     if (!subscriptionUrl) return;
